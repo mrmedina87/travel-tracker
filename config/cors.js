818268b0ee74e0909112781ec6bc57512316d5a4
mrmedina87 <mr_medina87@yahoo.com.ar>
2017-09-11 09:27:42 +0200

    cors

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ function crosPermission(){
   this.permission=function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods','GET','POST','PUT','DELETE','OPTIONS');
+    res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
     next();
   }
 }
